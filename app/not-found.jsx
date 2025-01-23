a93d59d35289a0600ed826974b293d722f580561
Abubakar <abubakarmurtala858@gmail.com>
2025-01-23 11:39:58 -0800

    second chance

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -2,10 +2,10 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <section className='py-24 bg-indigo-700'>
-      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center'>
+    <section className='py-36 h-screen bg-indigo-700'>
+      <div className='max-w-4xl mx-auto px-6 lg:px-8 flex flex-col'>
         <p className='text-white font-bold text-lg'>Opps!</p>
-        <h1 className='text-white text-5xl md:text-9xl font-bold'>404</h1>
+        <h1 className='text-white text-6xl md:text-9xl font-bold'>404</h1>
         <p className='text-white font-bold text-lg mb-3'>Sorry we could'nt find that page.</p>
         <p className='text-white'>
           From here you can either check out the <span>
